1819bcbf4f7a920463eed3a8a9cc5d24b9bec37d
zhangyan <tongkusat123@gmail.com>
2016-12-24 14:43:40 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -61,7 +61,7 @@ private
 def find_group_and_check_permission
 
   @group = Group.find(params[:id])
-  if current_user!==@group.user
+  if current_user!=@group.user
     redirect_to root_path, alert: "you have no permission."
   end
 end
